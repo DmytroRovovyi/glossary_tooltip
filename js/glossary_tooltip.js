36c4ef1cd072a344636c9665fffe4f00ed3ccbf1
Dmytro <06hannibal@gmail.com>
2025-01-14 23:16:30 +0200

    create new module

---
+++ js/glossary_tooltip.js
@@ -0,0 +1,43 @@
+(function ($, Drupal) {
+  Drupal.behaviors.glossaryTooltip = {
+    attach: function (context, settings) {
+      $('.glossary-tooltip', context).once('glossaryTooltip').click(function (e) {
+
+        // Get term values.
+        var description = $(this).data('description');
+        var termId = $(this).data('term-id');
+        var termUrl = '/taxonomy/term/' + termId;
+
+        // Description limit to 100 characters.
+        var truncatedDescription = description.length > 100 ? description.substring(0, 100) + '...' : description;
+
+        // add "Read more".
+        if (description.length > 100) {
+          truncatedDescription += '<br><a href="' + termUrl + '" class="read-more-glossary" target="_blank">Read more ' +
+            '<img src="/modules/custom/glossary_tooltip/svg/arrow-icon.svg" alt="arrow" width="13" height="13">' +
+            '</a>';
+        }
+
+        $('.glossary-popup').remove();
+
+        var $popup = $('<div class="glossary-popup">' + truncatedDescription + '</div>');
+        $('body').append($popup);
+
+        $popup.css({
+          top: e.pageY + 10,
+          left: e.pageX + 10
+        }).fadeIn();
+
+        e.preventDefault();
+      });
+
+      $(document).click(function (e) {
+        if (!$(e.target).closest('.glossary-tooltip, .glossary-popup').length) {
+          $('.glossary-popup').fadeOut(function () {
+            $(this).remove();
+          });
+        }
+      });
+    }
+  };
+})(jQuery, Drupal);
